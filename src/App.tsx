e80d41eb70f98c5e90d0eea740faaaf1f3bcc24a
toledanomartin <toledanomartin33@gmail.com>
2025-01-04 13:43:06 +0100

    fix router

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="score-swinger">
         <Routes>
           <Route path="/" element={<Setup />} />
           <Route path="/play" element={<Play />} />
